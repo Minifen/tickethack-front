03ea87b04b4c79391122d90e0067b0cc8ac2b07e
Your nameNicolas <Your email>
2023-11-07 18:38:47 +0100

    resolve endless conflicts

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-document.querySelector('#btn-search').addEventListener('click', function(){
-    
-    const travelSearch = {
-        departure : "Paris" ,
-        arrival :" Lyon",
-    }
 
-fetch('http://localhost:3000/trips/search',{
-
-
-    
-method: 'POST',
-headers: { 'Content-Type': 'application/json' },
-body: JSON.stringify(travelSearch)
-})
-.then(response => response.json())
-.then(data => {
-console.log(data);
-})
-
-
-});
-=======
 document.querySelector('#btn-search').addEventListener ('click', function() {
     
     const travelSearch = {
@@ -40,4 +17,4 @@ document.querySelector('#btn-search').addEventListener ('click', function() {
     console.log('gagné');
 })
 });
->>>>>>> a893335184bc21788433d820e1f65bfa6a363291
+
