808d2727a191ac0131d56411e341dbe64ac285f9
Quentin Fenaert <q.fenaert19@gmail.com>
2023-11-07 18:34:50 +0100

    modif script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelector('#btn-search').addEventListener('click', function(){
+    
+    const travelSearch = {
+        departure : "Paris" ,
+        arrival :" Lyon",
+    }
+
+fetch('http://localhost:3000/trips/search',{
+
+
+    
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify(travelSearch)
+})
+.then(response => response.json())
+.then(data => {
+console.log(data);
+})
+
+
+});
