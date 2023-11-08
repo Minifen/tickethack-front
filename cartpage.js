e446078d4855b4d9a93493c86525b65cbe1ac836
Your nameNicolas <Your email>
2023-11-08 15:22:14 +0100

    delete purchase

--- cartpage.js
+++ cartpage.js
@@ -5,20 +5,28 @@ fetch('http://localhost:3000/trips/totalCart')
 
 for (let i = 0; i<data.length; i++ ){
 document.querySelector('#Achats').innerHTML += `
-
-
-<p> ${data[i].trajet}</p>
-<p>${data[i].date}</p>
-<p>${data[i].price}</p>
-<button id="deleteCart" type="button">X</button>
-
-
-
-
+    <p>${data[i].trajet}</p>
+    <p class="p1">${data[i].date}</p>
+    <p classs="p2">${data[i].price}</p>
+    <button class="deleteCart" type="button">X</button>
 `
-
 }
-
-
-
 })
+
+const allDelete= document.querySelectorAll('.deleCart')
+
+    for (let i=0; i < allDelete.length; i++) {
+    allDelete[i].addEventListener ('click', function() {
+    
+        const deleteCart = {
+        trajet: document.querySelector('#Achats').firstElementChild.textContent,
+        date: document.querySelector('.p1').value,
+        price: document.querySelector('.p2').textContent,
+    }
+        fetch('http://localhost:3000/trips/deletetrip', {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(deleteCart)
+        })
+    })
+}
