79634878464427180d67a230d821d0b41f6d53a6
Your nameNicolas <Your email>
2023-11-08 17:11:37 +0100

    css index

--- cartpage.js
+++ cartpage.js
@@ -5,21 +5,21 @@ fetch('http://localhost:3000/trips/totalCart')
 
 for (let i = 0; i<data.length; i++ ){
 document.querySelector('#Achats').innerHTML += `
-    <p>${data[i].trajet}</p>
+    <p class="p0">${data[i].trajet}</p>
     <p class="p1">${data[i].date}</p>
-    <p classs="p2">${data[i].price}</p>
+    <p class="p2">${data[i].price}</p>
     <button class="deleteCart" type="button">X</button>
 `
 }
 })
 
-const allDelete= document.querySelectorAll('.deleCart')
+const allDelete= document.querySelectorAll('.deleteCart')
 
     for (let i=0; i < allDelete.length; i++) {
     allDelete[i].addEventListener ('click', function() {
     
         const deleteCart = {
-        trajet: document.querySelector('#Achats').firstElementChild.textContent,
+        trajet: document.querySelector('.p0').firstElementChild.textContent,
         date: document.querySelector('.p1').value,
         price: document.querySelector('.p2').textContent,
     }
