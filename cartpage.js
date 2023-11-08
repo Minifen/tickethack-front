b3281bfbfa419a44df48cfc873e2a2496dd608cb
Quentin Fenaert <q.fenaert19@gmail.com>
2023-11-08 14:59:52 +0100

    modif fichier front cart

--- cartpage.js
+++ cartpage.js
@@ -1,13 +1,17 @@
 fetch('http://localhost:3000/trips/totalCart')
 .then(response => response.json())
 .then(data => {
+
+
 for (let i = 0; i<data.length; i++ ){
 document.querySelector('#Achats').innerHTML += `
+
 
 <p> ${data[i].trajet}</p>
 <p>${data[i].date}</p>
 <p>${data[i].price}</p>
 <button id="deleteCart" type="button">X</button>
+
 
 
 
